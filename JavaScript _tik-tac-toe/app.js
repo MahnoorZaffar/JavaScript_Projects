69c9429a3d6759d0d6c1f828e6668d174e37a923
MahnoorZaffar <78152363+MahnoorZaffar@users.noreply.github.com>
2021-07-19 01:06:14 +0500

    Add files via upload

---
+++ JavaScript _tik-tac-toe/app.js
@@ -0,0 +1,97 @@
+const boxes = Array.from(document.getElementsByClassName('box'));
+const playText = document.getElementById('playText')
+const restartBtn = document.getElementById('restartBtn')
+const spaces = [] ;
+const O_TEXT = "0";
+const X_TEXT = "X";
+let currentPlayer;
+
+console.log(boxes);
+
+const drawBoard = () => {
+    boxes.forEach((box, index) =>{
+        let styleString = '';
+        if (index < 3) {
+            styleString += 'border-bottom: 3px solid var(--purple);';
+        }
+        if (index % 3 === 0) {
+            styleString += 'border-right: 3px solid var(--purple);';
+        }
+        if (index % 3 === 2) {
+            styleString += 'border-left: 3px solid var(--purple);';
+        }
+         if (index > 5) {
+            styleString += 'border-top: 3px solid var(--purple);';
+        }
+        box.style = styleString;
+        box.addEventListener('click', boxClicked);
+    })
+}
+const boxClicked = (e) => {
+    const id = e.target.id; 
+    console.log('box clicked');
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        if(playerHasWon()){
+            playText.innerText = '${currentPlayer variable} has Won!';
+            return;
+
+        }
+        currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+
+    }
+    
+}
+const playerHasWon = () => {
+    if(spaces[0] === currentPlayer) {
+        if(spaces[1] === currentPlayer && spaces[2] === currentPlayer){
+            console.log(`${currentPlayer} Wins Up Top!`);
+            return true;
+        }
+        if(spaces[3] === currentPlayer && spaces[6] === currentPlayer){
+            console.log(`${currentPlayer} Wins On The Left!`);
+            return true;
+        }
+        if(spaces[4] === currentPlayer && spaces[8] === currentPlayer){
+            console.log(`${currentPlayer} Wins Diagonally!`);
+            return true;
+        }
+        
+    } else if(spaces[8] === currentPlayer) {
+        if(spaces[2] === currentPlayer && spaces[5] === currentPlayer){
+            console.log(`${currentPlayer} Wins On The Right!`);
+            return true;
+        }
+        if(spaces[6] === currentPlayer && spaces[7] === currentPlayer){
+            console.log(`${currentPlayer} Wins On The Bottom!`);
+            return true;
+        }
+    }
+    if(spaces[4] ===currentPlayer){
+        if(spaces[1] === currentPlayer && spaces[7] === currentPlayer){
+            console.log(`${currentPlayer} Wins On The Right!`);
+            return true;
+        }
+        if(spaces[3] === currentPlayer && spaces[5] === currentPlayer){
+            console.log(`${currentPlayer} Wins On The Bottom!`);
+            return true;
+        }
+    }
+}
+
+const restart = () => {
+    spaces.forEach((space, index) => {
+        spaces[index] = null;
+    })
+    boxes.forEach(box => {
+        box.innerText = '';
+    })
+    playText.innerText = `Let's play!`;
+    currentPlayer = O_TEXT;
+}
+restartBtn.addEventListener('click', restart);
+
+restart();
+drawBoard();
